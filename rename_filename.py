92c6bfc08c564f3d0125d940c351100eaa9762ad
Ssduckk <dlwhdrlf0619@naver.com>
2022-02-23 16:10:01 +0900

    bag directory rename->cpy

--- rename_filename.py
+++ rename_filename.py
@@ -1,6 +1,6 @@
 import os, glob
 import natsort
-
+import shutil
 
 def rename_filename(src, global_count):
     dir_name = glob.glob(os.path.join(src, '*', '*'))
@@ -17,8 +17,8 @@ def rename_filename(src, global_count):
                 #os.rename(image, )
 
 
-def create_directory(src):
-    dir_name = glob.glob(os.path.join(src, '*'))
+def create_bag_directory(src):
+    dir_name = natsort.natsorted(glob.glob(os.path.join(src, '*')))
     for date_count in dir_name:
         date = date_count.split('/')[-1:][0]
         resolution_dir = glob.glob(os.path.join(date_count, '*'))
@@ -29,17 +29,26 @@ def create_directory(src):
                 count = 1
                 region = region_count.split('/')[-1:][0]
                 file_dir = glob.glob(os.path.join(region_count, '*'))
+                file_dir = natsort.natsorted(file_dir)
+                new_file_dir = 'rosbag/' + date + '_' + region
+                os.makedirs(os.path.join(src, new_file_dir), exist_ok=True)
                 for file in file_dir:
                     count_str = str(count).zfill(4)
+                    #print(resolution_count)
                     bag_name = file
                     count = count + 1
                     renamed_file = date + '_' + resolution + '_' + region + '_' + count_str + '.bag'
-                    os.rename(bag_name, renamed_file)
+                    shutil.copyfile(bag_name, os.path.join(src, new_file_dir, renamed_file))
 
+
+def create_img_directory(src):
+    dir_name = natsort.natsorted(glob.glob(os.path.join(src, '*')))
+    print(dir_name)
 
 if __name__ == "__main__":
     src_img = "/home/ri/workspace/test_image_picture"
-    src_dir = "/home/ri/workspace/test_bagfiles"
+    src_bag = "/home/ri/workspace/test_bagfiles"
     global_count = 1
     #rename_filename(src_img, global_count)
-    create_directory(src_dir)
+    create_bag_directory(src_bag)
+    #create_img_directory(src_img)
